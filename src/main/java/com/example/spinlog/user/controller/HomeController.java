e6be6148fcaceb50ff020cc4fa27023bb78cdcac
황중섭 <jshwang9709@gmail.com>
2024-05-16 11:50:58 +0900

    feat: 테스트용 컨트롤러 추가

--- src/main/java/com/example/spinlog/user/controller/HomeController.java
+++ src/main/java/com/example/spinlog/user/controller/HomeController.java
@@ -1,14 +1,14 @@
 package com.example.spinlog.user.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController { //TODO 배포 시 삭제
 
     @GetMapping("/")
     public String homePage() {
-        return "home";
+        return "main route";
     }
 
 }
--- src/main/java/com/example/spinlog/user/controller/ProfileController.java
+++ src/main/java/com/example/spinlog/user/controller/ProfileController.java
@@ -1,13 +1,22 @@
 package com.example.spinlog.user.controller;
 
-import org.springframework.stereotype.Controller;
+import com.example.spinlog.global.security.oauth2.user.CustomOAuth2User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@Slf4j
 public class ProfileController { //TODO 배포 시 삭제
 
     @GetMapping("/profile")
-    public String profilePage() {
-        return "profile";
+    public String profilePage(@AuthenticationPrincipal CustomOAuth2User oAuth2User) {
+        log.info("profile page");
+
+        String name = oAuth2User.getName();
+        String provider = oAuth2User.getOAuth2Response().getProvider();
+
+        return provider + " " + name;
     }
 }
