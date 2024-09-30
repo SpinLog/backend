50d52c01f9fcd43e109906450a42d9909f76d74a
HSH <han41562@gmail.com>
2024-09-30 12:46:37 +0900

    fix: edit HashCacheService's bean name at CacheConfigration to test

--- src/test/java/com/example/spinlog/util/CacheConfiguration.java
+++ src/test/java/com/example/spinlog/util/CacheConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 @TestConfiguration
 public class CacheConfiguration {
     @Bean
-    public HashCacheService cacheService() {
+    public HashCacheService hashCacheService() {
         return new MockHashCacheService();
     }
 }
